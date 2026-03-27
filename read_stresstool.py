bac2b7b879a8e7bfdb4e7848e1d9d612914f07a1
Leo Guo <leoliguo3.14159@gmail.com>
2026-03-27 18:22:41 +0100

    localization

--- read_stresstool.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# xls = pd.ExcelFile("stresstool_update.xls")
-
-# file = pd.ExcelFile("myfile.xlsx")
-
-with pd.ExcelFile("stresstool_update.xls") as xls:
-    df1 = pd.read_excel(xls, "Kriging")
-    df2 = pd.read_excel(xls, "Quadratic")
